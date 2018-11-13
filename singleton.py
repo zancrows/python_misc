4ccbc8ac2e91b60c2e7a18025c5ce3dc88a02752
zancrows <zancrows@tutanota.com>
2018-11-13 12:07:15 +0100

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -6,7 +6,7 @@ class SingletonClass:
     
     def __new__(cls, *args, **kwargs):
         if cls not in cls._instance:
-            cls._instance[cls] = super(Singleton, cls).__new__(cls, *args, **kwargs)
+            cls._instance[cls] = super(SingletonClass, cls).__new__(cls, *args, **kwargs)
         return cls._instance[cls]
     
     def __init__(self):
